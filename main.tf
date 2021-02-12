ae3f7bbdda60cbd0cb520e57e953416f4d7ebafa
hema <hema1.balusu1994@gmail.com>
2021-02-12 03:02:31 -0800

    acess

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+resource "vault_aws_secret_backend" "aws" {
+  access_key = "AKIA2TYAN3PJ2XGMTNVS"
+  secret_key = "Si2/QutEIob3uRogBbpaUFczcUZXCD3JocRfHES/Y"
+}
 provider "aws" {
   region = "us-east-2"
 }
@@ -5,3 +9,4 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
