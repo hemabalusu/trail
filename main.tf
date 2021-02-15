519039298c6ab21a394f73fb882ab8ab1b2d4bde
hema <hema1.balusu1994@gmail.com>
2021-02-14 22:21:54 -0800

    ec2

--- main.tf
+++ main.tf
@@ -1,27 +1,25 @@
-provider "aws" {
-  region = "us-east-2"
-  access_key = "AKIA2TYAN3PJR6DYXJWQ"
-  secret_key = "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"
-}
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "Mainhemavpc"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "Mainhema-su-et"
+    Name = "HelloWorld"
   }
-}
-resource "aws_ami_from_instance" "example" {
-  name               = "terraform-example"
-  source_instance_id = "i-ami-0ebc8f6f580a04647"
 }
 
