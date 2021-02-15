a0b57e6117795a94f0b54569829dde46aeac48f8
hema <hema1.balusu1994@gmail.com>
2021-02-14 22:47:32 -0800

    ec2keysvpc

--- main.tf
+++ main.tf
@@ -4,6 +4,17 @@ provider "aws" {
   access_key = "AKIA2TYAN3PJR6DYXJWQ"
   secret_key = "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"
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
 data "aws_ami" "ubuntu" {
   most_recent = true
 
